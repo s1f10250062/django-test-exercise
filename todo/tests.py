a0cee10752cf6bf7c3d80a3e4e32fa381c43aca1
1f10250297 <s1f102502979@iniad.org>
2026-07-09 11:24:00 +0900

    削除機能のテストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -110,3 +110,20 @@ class TodoViewTestCase(TestCase):
         response = client.get('/1/')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_delete_task_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.post('/{}/delete'.format(task.pk))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/')
+        with self.assertRaises(Task.DoesNotExist):
+            Task.objects.get(pk=task.pk)
+
+    def test_delete_task_fail(self):
+        client = Client()
+        response = client.post('/1/delete')
+
+        self.assertEqual(response.status_code, 404)
